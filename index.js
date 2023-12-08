d2f80c3e9a4709eaceb0b42dd2ef140f30873814
jfasoltholmes <jfasoltholmes@gmail.com>
2023-12-07 21:31:13 -0800

    Formatted index.html to have all inputs needed and modified index.js to have true/false console logs

--- index.js
+++ index.js
@@ -9,13 +9,41 @@ button.addEventListener("click", (e) => {
 function handleFormSubmission(e) {
     e.preventDefault();
 
-    const input = document.getElementById("inputID");
-    const inputValue = input.value
+    // Gathers csv input value
+    const csv = document.getElementById("csvID");
+    const csvValue = csv.value
+    // Gathers email input value
+    const email = document.getElementById("emailID");
+    const emailValue = email.value
+    // Gathers header input value
+    const header = document.getElementById("headerID");
+    const headerValue = header.value
+    // Gathers body input value
+    const body = document.getElementById("bodyID");
+    const bodyValue = body.value
 
-    // Checks if inputValue is not undefined ("None" in python)...
-    if ( inputValue ) {
-        console.log(inputValue)
+    // Checks if csvValue is not undefined ("None" in python)...
+    if ( csvValue ) {
+        console.log(csvValue)
     } else {
-        console.log("Nope")
+        console.log("The csv file input is empty")
+    }
+    // Checks if emailValue is not undefined ("None" in python)...
+    if ( emailValue ) {
+        console.log(emailValue)
+    } else {
+        console.log("The email input is empty")
+    }
+    // Checks if headerValue is not undefined ("None" in python)...
+    if ( headerValue ) {
+        console.log(headerValue)
+    } else {
+        console.log("The header input is empty")
+    }
+    // Checks if bodyValue is not undefined ("None" in python)...
+    if ( bodyValue ) {
+        console.log(bodyValue)
+    } else {
+        console.log("The body input is empty")
     }
 }
